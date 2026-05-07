87e88d0211504d6d7fd9397840339e00bbb82ed7
Arseny <gorbanarseny@gmail.com>
2026-05-07 20:55:47 +0500

    'добавлены_сложности_и_категории_слов'

--- game.py
+++ game.py
@@ -3,27 +3,36 @@ import os
 import subprocess
 import time
 import random
+import ast
+import json
 
 difs = {'1': 'easy.txt', '2': 'medium.txt', '3': 'hard.txt'}
 diff = {'1': 'легкая сложность', '2': 'умеренная сложность', '3': 'высокая сложность'}
 
-def printscreen(res,gamedif,lifes,used):
+def printscreen(res,gamedif,lifes,used,category):
     os.system('cls')
     print('ctrl + c - для закрытия программы')
     print(diff[gamedif])
+    print(f'тема загаданного слова :  {category}')
     print(*res)
     print('проверенные буквы : ',*used)
     print(f'осталось попыток {lifes}')
 
+def get_word(gamedif):
+    with open(os.path.join('words', difs[gamedif]), 'r', encoding='utf-8') as f:
+        d = ast.literal_eval(f.read())
+        category = random.choice(list(d.keys()))
+        word = random.choice(d[category].strip().split(', '))
+        return category,word
+
 def gamestart(gamedif):
     word = ''
     used = []
-    with open(os.path.join('words',difs[gamedif]),'r',encoding='utf-8') as f:
-        word = random.choice(f.read().strip().split())
+    category,word = get_word(gamedif)
     res = ['_' for i in range(len(word))]
-    lifes = 5
+    lifes = 10
     while lifes != 0 and '_' in res:
-        printscreen(res,gamedif,lifes,used)
+        printscreen(res,gamedif,lifes,used,category)
         l = input('введите одну букву : ')
 
         if len(l) != 1:
@@ -39,15 +48,12 @@ def gamestart(gamedif):
         else:
             used.append(l)
 
-        pos = word.find(l)
-        if pos == -1:
-            lifes -= 1
+        if l in word:
+            for i in range(len(word)):
+                if word[i] == l:
+                    res[i] = l  # открываем ВСЕ одинаковые буквы
         else:
-            res[pos] = l
-            pos = word[pos+1:].find(l)
-            while pos != -1 and pos < len(word):
-                res[pos] = l
-                pos = word[pos+1:].find(l)
+            lifes -= 1
     print('#########################################')
     if lifes == 0:
         print('вы проиграли (((')
@@ -64,9 +70,10 @@ if __name__ == '__main__':
         os.system('cls')
         print('выберете сложность', '1 - легко', '2 - средне', '3 - сложно', '4 - выход', sep = '\n')
         game = input()
-        if game == '1':
+        if game in '123':
             gamestart(game)
         elif game == '4':
+            os.system('cls')
             sys.exit()
         else:
             os.system('cls')
